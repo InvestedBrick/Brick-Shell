6bda44525f0758eeea472628f338ad5b0f9a7e3d
InvestedBrick <julianbrecht25@gmail.com>
2025-02-02 20:58:01 +0100

    Added hinting for commonly used commands

--- src/autocomplete.rs
+++ src/autocomplete.rs
@@ -9,47 +9,86 @@ use rustyline::validate::Validator;
 use std::borrow::Cow;
 use std::env;
 use std::fs;
+
+use crate::commons;
+use commons::{read_commons,get_home_usr};
 pub struct FileCompleter;
 
+fn get_dir_and_input(line: &str, pos: usize) -> (usize,String,String,String){
+    let start = line[..pos].rfind(' ').map_or(0, |i| i + 1);
+    let mut input = &line[start..pos];
+    let mut first = String::new();
+    // Get the current directory
+    let mut dir = env::current_dir().unwrap().display().to_string();
+    if input.contains("/"){
+        let (first_part,second_part) = input.split_at(input.rfind('/').unwrap());
+        input = &second_part[1..];
+        first = first_part.to_owned() + "/";
+        dir = dir + "/" + &first;
+    }
+
+    (start,input.to_string(),dir,first)
+}
+
+fn hint_dir_item(line: &str, pos: usize, _ctx: &Context) -> Option<String>{
+    // Find the position where the filename starts
+    let (_,input, dir,_) = get_dir_and_input(line, pos);
+    // Get matching file names in the target directory
+    match fs::read_dir(&dir) {
+        Ok(entries) => 
+            for entry in entries{
+
+                if entry.is_ok() {
+                    let e = entry.unwrap();
+                    let mut file_name = e.file_name().to_string_lossy().to_string();
+                    if file_name.starts_with(&input) && !file_name.ends_with(".tmp") && !file_name.starts_with(".") && input != ""{
+                        file_name = file_name[file_name.find(&input).unwrap() + input.len()..].to_string();
+                        if e.file_type().unwrap().is_dir(){
+                            return Some(file_name + "/");
+                        }else{
+                            return Some(file_name);
+                        }
+                    } 
+                }
+            }
+            
+            
+        Err(_) => return None
+    };
+
+    return None;
+}
+
+fn complete_dir_item(line: &str, pos: usize, _ctx: &Context) -> (usize,Vec<String>){
+    let (start,input,dir,first) = get_dir_and_input(line, pos);
+    // Get matching file names in the target directory
+    let files = match fs::read_dir(&dir) {
+        Ok(entries) => entries
+            .filter_map(|entry| {
+                entry.ok().and_then(|e| {
+                    let file_name = e.file_name().to_string_lossy().to_string();
+                    if file_name.starts_with(&input[..]) {
+                        if e.file_type().unwrap().is_dir(){
+                            Some(first.to_owned() + &file_name + "/")
+                        }else{
+                            Some(first.to_owned() + &file_name)
+                        }
+                    } else {
+                        None
+                    }
+                })
+            })
+            .collect(),
+        Err(_) => vec![],
+    };
+    (start,files)
+}
 impl Completer for FileCompleter {
     type Candidate = String;
 
     fn complete(&self, line: &str, pos: usize, _ctx: &Context) -> rustyline::Result<(usize, Vec<Self::Candidate>)> {
-        // Find the position where the filename starts
-        let start = line[..pos].rfind(' ').map_or(0, |i| i + 1);
-        let mut input = &line[start..pos];
-        
-        // Get the current directory
-        let mut first = String::new();
-        let mut dir = env::current_dir().unwrap().display().to_string();
-        if input.contains("/"){
-            let (first_part,second_part) = input.split_at(input.rfind('/').unwrap());
-            input = &second_part[1..];
-            first = first_part.to_owned() + "/";
-            dir = dir + "/" + &first;
-        }
-        // Get matching file names in the target directory
-        let files = match fs::read_dir(&dir) {
-            Ok(entries) => entries
-                .filter_map(|entry| {
-                    entry.ok().and_then(|e| {
-                        let file_name = e.file_name().to_string_lossy().to_string();
-                        if file_name.starts_with(input) {
-                            if e.file_type().unwrap().is_dir(){
-                                Some(first.to_owned() + &file_name + "/")
-                            }else{
-                                Some(first.to_owned() + &file_name)
-                            }
-                        } else {
-                            None
-                        }
-                    })
-                })
-                .collect(),
-            Err(_) => vec![],
-        };
+        let (start,files) = complete_dir_item(line, pos, _ctx);
 
-        // Return completion candidates
         Ok((start, files))
     }
 }
@@ -74,41 +113,20 @@ impl Highlighter for FileCompleter {
 impl Hinter for FileCompleter {
     type Hint = String;
     fn hint(&self, line: &str, pos: usize, _ctx: &Context) -> Option<String> {
-        // Find the position where the filename starts
-        let start = line[..pos].rfind(' ').map_or(0, |i| i + 1);
-        let mut input = &line[start..pos];
-        
-        // Get the current directory
-        let mut dir = env::current_dir().unwrap().display().to_string();
-        if input.contains("/"){
-            let (first_part,second_part) = input.split_at(input.rfind('/').unwrap());
-            input = &second_part[1..];
-            dir = dir + "/" + first_part + "/";
-        }
-        // Get matching file names in the target directory
-        match fs::read_dir(&dir) {
-            Ok(entries) => 
-                for entry in entries{
+        if line != "" && (line.find(" ").is_none() || (line.rfind(" | ").is_some() && line.rfind(" ").unwrap() == pos)) {
+            let start = line[..pos].rfind(' ').map_or(0, |i| i + 1);
+            let input = &line[start..pos];
+            let commons = read_commons(get_home_usr());
 
-                    if entry.is_ok() {
-                        let e = entry.unwrap();
-                        let mut file_name = e.file_name().to_string_lossy().to_string();
-                        if file_name.starts_with(input) && !file_name.ends_with(".tmp") && !file_name.starts_with(".") && input != ""{
-                            file_name = file_name[file_name.find(input).unwrap() + input.len()..].to_string();
-                            if e.file_type().unwrap().is_dir(){
-                                return Some(file_name + "/");
-                            }else{
-                                return Some(file_name);
-                            }
-                        } 
-                    }
+            for common in commons{
+                if common.starts_with(&input){
+                    return Some(common[common.find(&input).unwrap() + input.len()..].to_string());
                 }
-                
-                
-            Err(_) => return None
-        };
-
-        return None;
+            }
+            None
+        }else{
+            hint_dir_item(line, pos, _ctx)
+        }
     }
 }
 
