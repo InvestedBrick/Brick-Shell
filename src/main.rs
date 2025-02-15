bf673e152e352b6efe5bf909997bf5ffe4557750
InvestedBrick <julianbrecht25@gmail.com>
2025-02-15 19:46:30 +0100

    Added command chaining

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,32 @@ use commons::{read_commons,write_commons,get_home_usr};
 use std::collections::HashMap;
 mod aliases;
 use aliases::{read_aliases,write_aliases};
-
+pub fn split_with_delimiter(s: &str, delimiter: &str) -> Vec<String> {
+    let mut result:Vec<String> = Vec::new();
+    if !s.contains(delimiter){
+        result.push(s.to_string());
+        return result;
+    }
+    let mut start = 0;
+    let mut in_quotes = false;
+    let mut i = 0;
+    
+    while i <= s.len() - delimiter.len() {
+        if s[i..].starts_with(delimiter) && !in_quotes {
+            result.push(s[start..i].to_string());
+            i += delimiter.len();
+            start = i;
+        } else {
+            if s[i..].starts_with('"') {
+                in_quotes = !in_quotes;
+            }
+            i += 1;
+        }
+    }
+    
+    result.push(s[start..].to_string()); // Push the remaining part
+    result
+}
 fn split_args(command : &str) -> (&str, IntoIter<String>){
     if let Some((command_,args_)) = command.split_once(' '){
         let mut result_args: Vec<String> = Vec::new();
@@ -65,7 +90,7 @@ fn main_shell() -> bool{
 
     let mut aliases = read_aliases(alias_file.clone());
     let mut perm_aliases = HashMap::<String,bool>::new();
-
+    let mut alias_vec: Vec<String> = Vec::new();
     for key in aliases.keys(){
         perm_aliases.insert(key.to_string(), true);
     }
@@ -110,170 +135,178 @@ fn main_shell() -> bool{
             }
         };
             
-        //stdin().read_line(&mut input).unwrap();
         rl.add_history_entry(&input).unwrap();
-        let mut commands = input.trim().split(" | ").peekable();
-        let mut prev_command:Option<Child>  = None;
-        let mut read_from_map = false;
-        let mut alias_key = String::new();
-        while let Some(command) = if read_from_map {aliases.get(&alias_key).map(|s|s.as_str())} else {commands.next()} {
-            let (command,args) = split_args(command.trim());
-            read_from_map = false;
-            match command {
-
-                "cd" => {
-                    let new_dir = args.peekable().peek().map_or(home_dir().unwrap().display().to_string() , |x| x.to_string());
-
-                    let path = Path::new(&new_dir);
-
-                    if let Err(e) = env::set_current_dir(&path){
-                        eprintln!("{}",e);
-                    }
-
-                    prev_command = None;
-                },
-                "exit"  => {
-                    rl.append_history(&hist_file).unwrap();
-                    write_commons(home_usr, commons);
-                    write_aliases(alias_file, &aliases.into_iter().filter(|(key,_)| perm_aliases.get(key).copied().unwrap_or(false)).collect());
-                    return false;
-                },
-                "restart"  => { // not pretty but I dont wanna deal with borrowing command stuff
-                    rl.append_history(&hist_file).unwrap();
-                    write_commons(home_usr, commons);
-                    write_aliases(alias_file, &aliases.into_iter().filter(|(key,_)| perm_aliases.get(key).copied().unwrap_or(false)).collect());
-                    return true;
-                },
-                "clear-history" => {
-                    if rl.clear_history().is_err() {
-                        eprintln!("Failed to clear history");
-                    }
-                    
-                    fs::remove_file(&hist_file).unwrap();
-                }
-                "alias" => {
-                    let mut args = args.peekable();
-
-                    let arg = args.peek().map_or(String::new(), |x| x.to_string());
-                    if !(arg == "-t" || arg == "-p" || arg == ""){
-                        eprintln!("Invalid Argument! Can only use '-t' for temporary and '-p' for permanent aliases");
-                        continue;
-                    }
-                    args.next();
-                    let perm = arg == "-p";
-
-                    let alias = args.next().unwrap_or_default();
-                    if alias.is_empty() {
-                        eprintln!("Expected alias");
-                        continue;
-                    }
-
-                    let alias_content = args.next().unwrap_or_default();
-                    if alias_content.is_empty() {
-                        eprintln!("Expected what '{}' is supposed to be an alias for", alias);
-                        continue;
-                    }
-
-                    aliases.insert(alias.clone(), alias_content);
-                    perm_aliases.insert(alias, perm);
-                        
-                }
-                "ls" => {
-                    let arg = args.peekable().peek().map_or(String::new(), |x| x.to_string());
-                    if !(arg == "-e" || arg == "-a" || arg == ""){
-                        eprintln!("Invalid Argument! Can only use '-e' for extended and '-a' for all-view");
-                        continue;
-                    }
-                    let extended = arg == "-e";
-                    let all = arg == "-a";
-                    let files = fs::read_dir(&dir).unwrap();
-                    let mut ls_output = String::new();
-                    for file in files {
-                        let file_type = file.as_ref().unwrap().file_type().unwrap();
-                        let file_name = file.unwrap().file_name().into_string().unwrap();
-                        let starts_with_dot = file_name.starts_with(".");
-                        if file_type.is_dir() {
-                            if starts_with_dot && (extended || all){
-                                ls_output.push_str(&format!("{}/\n",file_name.purple().bold()));
-                            }else if !starts_with_dot{
-                                ls_output.push_str(&format!("{}/\n",file_name.blue().bold()));
-                            }
-                        }else if file_type.is_file() {
-                            if !file_name.ends_with(".tmp") || all{
-                                
-                                if starts_with_dot && (extended || all){
-                                    ls_output.push_str(&format!("{}\n",file_name));
-                                }
-                                else if !starts_with_dot && file_name.contains("."){
-                                    ls_output.push_str(&format!("{}\n",file_name));
-                                }else if !starts_with_dot {
-                                    ls_output.push_str(&format!("{}\n",file_name.green().bold()));
-                                }
-                            }
-                        }else if file_type.is_symlink() {
-                            ls_output.push_str(&format!(">{}\n",file_name.bright_cyan()));
-                        }
-                    }
-                    // No pipe -> print
-                    if commands.peek().is_none() {
-                        print!("{}",ls_output);
-                    }else{
-                        let (reader,mut writer) = pipe().expect("Pipe failed");
-                        writer.write_all(ls_output.as_bytes()).unwrap();
-                        
-                        drop(writer);
-
-                        prev_command = Some(
-                            Command::new("cat") 
-                            .stdin(Stdio::from(reader))
-                            .stdout(Stdio::piped())
-                            .spawn()
-                            .expect("Failed to spawn command")
-                        );
-
-                        
-                    }
-                },
-                command => {
-                    
-                    let stdin = prev_command
-                        .map_or(
-                            Stdio::inherit(),
-                            |output: Child| Stdio::from(output.stdout.unwrap())
-                        );
-                    let stdout = if commands.peek().is_some() {
-                            Stdio::piped()
-                        } else {
-                            Stdio::inherit()
-                        };
-                    
-                    let output = Command::new(command)
-                    .args(args)
-                    .stdin(stdin)
-                    .stdout(stdout)
-                    .spawn();
+        //let commands_chain = input.trim().split(" && ").peekable();
+        let commands: Vec<String> = split_with_delimiter(&input.trim(), " && ");
+        println!("{:?}",commands);
+        for c in commands {
+            let split_iter:Vec<String> = split_with_delimiter(&c.trim(), " | ");
+            let mut split_commands = split_iter.into_iter().peekable();
+            
+            let mut prev_command: Option<Child>  = None;
+            
+            while let Some(command) = alias_vec.pop().or_else(||split_commands.next())  {
+                let (command,args) = split_args(command.trim());
+                println!("running '{}' with args: {:?}",command,args);
+                match command {
                 
-                match output {
-                    Ok(output) => {prev_command = Some(output); if !commons.contains(&command.to_string()) {commons.push(command.to_string() )};}, // Only push to commons if true command 
-                    Err(_e) => {
-                        prev_command = None; 
-                        if aliases.contains_key(command){
-                            if !commons.contains(&command.to_string()) {commons.push(command.to_string());} // push custom commands
-                            read_from_map = true;
-                            alias_key = command.to_string();
+                    "cd" => {
+                        let new_dir = args.peekable().peek().map_or(home_dir().unwrap().display().to_string() , |x| x.to_string());
+                    
+                        let path = Path::new(&new_dir);
+                        
+                        if let Err(e) = env::set_current_dir(&path){
+                            eprintln!("{}",e);
+                        }
+                    
+                        prev_command = None;
+                    },
+                    "exit"  => {
+                        rl.append_history(&hist_file).unwrap();
+                        write_commons(home_usr, commons);
+                        write_aliases(alias_file, &aliases.into_iter().filter(|(key,_)| perm_aliases.get(key).copied().unwrap_or(false)).collect());
+                        return false;
+                    },
+                    "restart"  => { // not pretty but I dont wanna deal with borrowing command stuff
+                        rl.append_history(&hist_file).unwrap();
+                        write_commons(home_usr, commons);
+                        write_aliases(alias_file, &aliases.into_iter().filter(|(key,_)| perm_aliases.get(key).copied().unwrap_or(false)).collect());
+                        return true;
+                    },
+                    "clear-history" => {
+                        if rl.clear_history().is_err() {
+                            eprintln!("Failed to clear history");
+                        }
+                        
+                        fs::remove_file(&hist_file).unwrap();
+                    }
+                    "alias" => {
+                        let mut args = args.peekable();
+                    
+                        let arg = args.peek().map_or(String::new(), |x| x.to_string());
+                        if !(arg == "-t" || arg == "-p" || arg == ""){
+                            eprintln!("Invalid Argument! Can only use '-t' for temporary and '-p' for permanent aliases");
                             continue;
+                        }
+                        args.next();
+                        let perm = arg == "-p";
+                        
+                        let alias = args.next().unwrap_or_default();
+                        if alias.is_empty() {
+                            eprintln!("Expected alias");
+                            continue;
+                        }
+                        
+                        let alias_content = args.next().unwrap_or_default();
+                        if alias_content.is_empty() {
+                            eprintln!("Expected what '{}' is supposed to be an alias for", alias);
+                            continue;
+                        }
+                    
+                        aliases.insert(alias.clone(), alias_content);
+                        perm_aliases.insert(alias, perm);
+                            
+                    }
+                    "ls" => {
+                        let arg = args.peekable().peek().map_or(String::new(), |x| x.to_string());
+                        if !(arg == "-e" || arg == "-a" || arg == ""){
+                            eprintln!("Invalid Argument! Can only use '-e' for extended and '-a' for all-view");
+                            continue;
+                        }
+                        let extended = arg == "-e";
+                        let all = arg == "-a";
+                        let files = fs::read_dir(&dir).unwrap();
+                        let mut ls_output = String::new();
+                        for file in files {
+                            let file_type = file.as_ref().unwrap().file_type().unwrap();
+                            let file_name = file.unwrap().file_name().into_string().unwrap();
+                            let starts_with_dot = file_name.starts_with(".");
+                            if file_type.is_dir() {
+                                if starts_with_dot && (extended || all){
+                                    ls_output.push_str(&format!("{}/\n",file_name.purple().bold()));
+                                }else if !starts_with_dot{
+                                    ls_output.push_str(&format!("{}/\n",file_name.blue().bold()));
+                                }
+                            }else if file_type.is_file() {
+                                if !file_name.ends_with(".tmp") || all{
+                                    
+                                    if starts_with_dot && (extended || all){
+                                        ls_output.push_str(&format!("{}\n",file_name));
+                                    }
+                                    else if !starts_with_dot && file_name.contains("."){
+                                        ls_output.push_str(&format!("{}\n",file_name));
+                                    }else if !starts_with_dot {
+                                        ls_output.push_str(&format!("{}\n",file_name.green().bold()));
+                                    }
+                                }
+                            }else if file_type.is_symlink() {
+                                ls_output.push_str(&format!(">{}\n",file_name.bright_cyan()));
+                            }
+                        }
+                        // No pipe -> print
+                        if split_commands.peek().is_none() {
+                            print!("{}",ls_output);
                         }else{
-
-                            if command.to_string() != "" {
-                                eprintln!("Command '{}' was not found!",command.to_string())};
+                            let (reader,mut writer) = pipe().expect("Pipe failed");
+                            writer.write_all(ls_output.as_bytes()).unwrap();
+                            
+                            drop(writer);
+                            
+                            prev_command = Some(
+                                Command::new("cat") 
+                                .stdin(Stdio::from(reader))
+                                .stdout(Stdio::piped())
+                                .spawn()
+                                .expect("Failed to spawn command")
+                            );
+                            
+                            
+                        }
+                    },
+                    command => {
+                        
+                        let stdin = prev_command
+                        .map_or(
+                                Stdio::inherit(),
+                                |output: Child| Stdio::from(output.stdout.unwrap())
+                            );
+                        let stdout = if split_commands.peek().is_some() {
+                            Stdio::piped()
+                            } else {
+                                Stdio::inherit()
+                            };
+                            
+                            let output = Command::new(command)
+                            .args(args)
+                            .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+                        
+                    match output {
+                        Ok(output) => {prev_command = Some(output); if !commons.contains(&command.to_string()) {commons.push(command.to_string() )};}, // Only push to commons if true command 
+                        Err(_e) => {
+                            prev_command = None; 
+                            if aliases.contains_key(command){
+                                if !commons.contains(&command.to_string()) {commons.push(command.to_string());} // push custom commands
+                                let cmd = aliases.get(&command.to_string()).unwrap();
+                                let cmds = split_with_delimiter(&cmd.trim(), " && ");
+                                for c in cmds.iter().rev(){
+                                    alias_vec.push(c.to_string());
+                                }
+                                continue;
+                            }else{
+                                
+                                if command.to_string() != "" {
+                                    eprintln!("Command '{}' was not found!",command.to_string())};
+                                }
                             }
                         }
                     }
                 }
             }
-        }
-        if let Some(mut final_cmd) = prev_command {
-            final_cmd.wait().unwrap();
+            if let Some(mut final_cmd) = prev_command {
+                final_cmd.wait().unwrap();
+            }
         }   
     }
 
